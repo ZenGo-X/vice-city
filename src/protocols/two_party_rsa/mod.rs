44048bf5a7ce6776a0907ee5d62b41807dcc8f16
Omer Shlomovits <omer.shlomovits@gmail.com>
2020-10-19 20:18:46 +0300

    param change

--- src/protocols/two_party_rsa/mod.rs
+++ src/protocols/two_party_rsa/mod.rs
@@ -1,9 +1,9 @@
 pub mod hmrt;
 
 // for N with 2048 bits
-//const PAILLIER_MODULUS: usize = 3072;
-//const CANDIDATE_BIT_LENGTH: usize = 2048;
+const PAILLIER_MODULUS: usize = 3072;
+const CANDIDATE_BIT_LENGTH: usize = 2048;
 
 // testing : for N with 128 bit
-const PAILLIER_MODULUS: usize = 2048;
-const CANDIDATE_BIT_LENGTH: usize = 128;
+//const PAILLIER_MODULUS: usize = 2048;
+//const CANDIDATE_BIT_LENGTH: usize = 128;
